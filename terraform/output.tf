7580418eba9c829d5445469afe265025a4823111
Evgeny <evgenikk96@gmail.com>
2018-10-31 14:00:56 +0300

    basic terraform lab and 1st additional task

---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+#Выводим внешний адрес машинки
+output "app_external_ip" {
+  value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+#output "mykey" {
+#value = "${data.local_file.appuser_key.content}"
+#}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+project = "infra-219513"
+public_key_path = "/Users/Radio/.ssh/appuser/id_rsa.pub"
+disk_image = "reddit-base" 
+private_key_path = "/Users/Radio/.ssh/appuser/id_rsa"
