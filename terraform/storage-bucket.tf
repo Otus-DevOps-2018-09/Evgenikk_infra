9338339c81314dd0d4a64b07ca2b4550e2f14451
Evgeny <evgenikk96@gmail.com>
2018-11-04 12:38:15 +0300

    fmt base dz part

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,13 +1,15 @@
 provider "google" {
- version = "1.4.0"
- project = "${var.project}"
- region = "${var.region}"
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
+
 module "storage-bucket" {
- source = "SweetOps/storage-bucket/google"
- version = "0.1.1"
- name = ["evgeny-infra-bucket-1", "evgeny-infra-bucket-2"]
+  source  = "SweetOps/storage-bucket/google"
+  version = "0.1.1"
+  name    = ["evgeny-infra-bucket-1", "evgeny-infra-bucket-2"]
 }
+
 output storage-bucket_url {
- value = "${module.storage-bucket.url}"
+  value = "${module.storage-bucket.url}"
 }
