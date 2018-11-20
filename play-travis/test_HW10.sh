9b5a551e1eeff060db357f449d191bae9642fa33
Evgeny <evgenikk96@gmail.com>
2018-11-20 18:24:44 +0300

    add sleep

--- play-travis/test_HW10.sh
+++ play-travis/test_HW10.sh
@@ -2,26 +2,20 @@
 PROJECT_ROOT=`pwd`
 
 # Устанавливаем packer
-pwd
-ls -la
 echo "rm packer from workdir"
 sudo rm -rf packer*
-ls -la
 echo "download packer"
 sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
-ls -la
 echo "unzip packer"
 sudo unzip -o packer_1.3.1_linux_386.zip
-ls -la
 echo "delete  packer zip "
 sudo rm -f packer_1.3.1_linux_386.zip
-ls -la
 echo "delete old packer"
+sleep(5)
 sudo rm -rf /usr/local/bin/packer
-sudo ls -la /usr/local/bin
 echo "mv packer"
 sudo mv -f packer /usr/local/bin/
-sudo ls -la /usr/local/bin
+
 
 
 #Устанавливаем  ansible-lint
@@ -32,6 +26,7 @@ ansible-lint --version
 sudo wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_386.zip
 sudo unzip  -o terraform_*.zip
 sudo rm -f terraform_*.zip
+sleep(5)
 sudo mv -f /usr/local/src/terraform /usr/local/bin/
 
 # Устанавливаем  tflint
@@ -39,6 +34,7 @@ sudo mv -f /usr/local/src/terraform /usr/local/bin/
 sudo wget https://github.com/wata727/tflint/releases/download/v0.7.2/tflint_linux_386.zip
 sudo unzip -o tflint_*.zip
 sudo rm -f tflint_*.zip
+sleep(5)
 sudo mv -f  /usr/local/src/tflint /usr/local/bin/
 
 export PATH
