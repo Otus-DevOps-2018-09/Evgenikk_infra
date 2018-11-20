b369ceac2808974b0ebd8b69c06e23102ae74fdb
Evgeny <evgenikk96@gmail.com>
2018-11-20 14:52:34 +0300

    add -o option

--- play-travis/test_HW10.sh
+++ play-travis/test_HW10.sh
@@ -3,7 +3,7 @@ PROJECT_ROOT=`pwd`
 
 # Устанавливаем packer
 sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
-sudo unzip packer_1.3.1_linux_386.zip
+sudo unzip -o packer_1.3.1_linux_386.zip
 sudo rm -f packer_1.3.1_linux_386.zip
 sudo mv -f packer /usr/local/bin/
 
@@ -13,13 +13,13 @@ sudo pip install ansible-lint
 ansible-lint --version
 
 sudo wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_386.zip
-sudo unzip terraform_*.zip
+sudo unzip  -o terraform_*.zip
 sudo rm -f terraform_*.zip
 sudo mv -f /usr/local/src/terraform /usr/local/bin/
 
 # Устанавливаем  tflint
 sudo wget https://github.com/wata727/tflint/releases/download/v0.7.2/tflint_linux_386.zip
-sudo unzip tflint_*.zip
+sudo unzip -o tflint_*.zip
 sudo rm -f tflint_*.zip
 sudo mv -f  /usr/local/src/tflint /usr/local/bin/
 
