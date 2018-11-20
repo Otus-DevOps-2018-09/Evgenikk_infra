b850f28a53972881c22919da19225e8060478732
Evgeny <evgenikk96@gmail.com>
2018-11-20 14:29:54 +0300

    add packages installation in tests env

--- play-travis/test_HW10.sh
+++ play-travis/test_HW10.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 PROJECT_ROOT=`pwd`
 
+# Устанавливаем packer
+sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
+sudo unzip packer_1.3.1_linux_386.zip
+sudo rm -f packer_1.3.1_linux_386.zip
+sudo mv packer /usr/local/bin/
 
 #Устанавливаем  ansible-lint
 cd /usr/local/src
 sudo pip install ansible-lint
 ansible-lint --version
+
+sudo wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_386.zip
+sudo unzip terraform_*.zip
+sudo rm -f terraform_*.zip
+sudo mv /usr/local/src/terraform /usr/local/bin/
+
+# Устанавливаем  tflint
+sudo wget https://github.com/wata727/tflint/releases/download/v0.7.2/tflint_linux_386.zip
+sudo unzip tflint_*.zip
+sudo rm -f tflint_*.zip
+sudo mv /usr/local/src/tflint /usr/local/bin/
+
+echo '#######################################################################'
+echo 'Check version of installed packages'
 echo 'packer version:'
 packer version
 echo 'terraform  version:'
@@ -15,5 +34,7 @@ tflint -v
 
 cd $PROJECT_ROOT
 
-#Проверяем все playbook-и при помощи  ansible-lint
+
+echo '#######################################################################'
+echo 'ansible-lint tests:'
 ansible-lint --exclude=roles/jdauphant.nginx ansible/playbooks/*.yml
