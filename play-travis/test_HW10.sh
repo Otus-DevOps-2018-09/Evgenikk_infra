93a9face39e89d4d2e93f52953ed9849760381c8
Evgeny <evgenikk96@gmail.com>
2018-11-20 11:55:58 +0300

    add ansible-lint test script

---
+++ play-travis/test_HW10.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PROJECT_ROOT=`pwd`
+
+cd /usr/local/src
+pip install ansible-lint
+ansible-lint --version
+
+cd $PROJECT_ROOT
+
+echo '###############################'
+echo 'Ansible-lint test'
+ansible-lint --exclude=roles/jdauphant.nginx ansible/playbooks/*.yml
