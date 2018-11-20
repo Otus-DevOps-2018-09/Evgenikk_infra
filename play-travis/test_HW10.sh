687ce64bf23214f29ac0c6e71d58a8b8f205a5d8
Evgeny <evgenikk96@gmail.com>
2018-11-20 16:09:47 +0300

    add packer tests and fix ansible-lint

--- play-travis/test_HW10.sh
+++ play-travis/test_HW10.sh
@@ -41,3 +41,16 @@ cd $PROJECT_ROOT
 echo '#######################################################################'
 echo 'ansible-lint tests:'
 ansible-lint --exclude=roles/jdauphant.nginx ansible/playbooks/*.yml
+
+echo '#######################################################################'
+echo 'packer tests:'
+
+echo "Validate app.json"
+packer validate -var-file=packer/variables.json.example packer/app.json
+echo "Validate db.json"
+packer validate -var-file=packer/variables.json.example packer/db.json
+cd packer
+echo "Validate immutable.json"
+packer validate -var-file=variables.json.example immutable.json
+echo "Validate ubuntu16.json"
+packer validate -var-file=variables.json.example ubuntu16.json
