4a93b798a734b78848587ac5b653117ce413efac
Evgeny <evgenikk96@gmail.com>
2018-11-20 14:00:41 +0300

    add version checks

--- play-travis/test_HW10.sh
+++ play-travis/test_HW10.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 PROJECT_ROOT=`pwd`
 
+
+#Устанавливаем  ansible-lint
 cd /usr/local/src
 sudo pip install ansible-lint
 ansible-lint --version
+echo 'packer version:'
+packer --version
+echo 'terraform  version:'
+terraform --version
+echo 'tflint version:'
+tflint -v
 
 cd $PROJECT_ROOT
 
-echo '#######################################################################'
-echo 'Ansible-lint test'
+#Проверяем все playbook-и при помощи  ansible-lint
 ansible-lint --exclude=roles/jdauphant.nginx ansible/playbooks/*.yml
